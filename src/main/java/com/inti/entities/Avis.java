9165d9450976d442c51628e60ee2547469a4d83f
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2022-08-23 10:15:54 +0200

    test conflit

---
+++ src/main/java/com/inti/entities/Avis.java
@@ -0,0 +1,22 @@
+package com.inti.entities;
+
+public class Avis {
+	
+	private long idAvis;
+
+	public Avis() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getIdAvis() {
+		return idAvis;
+	}
+
+	public void setIdAvis(long idAvis) {
+		this.idAvis = idAvis;
+	}
+	
+	
+
+}
