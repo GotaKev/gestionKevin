482e0d971ced6ce9ae93dc89890f43a9e45722cb
GotaKev <kflesch.pro@gmail.com>
2022-08-23 10:14:59 +0200

    ajout avis kevin

---
+++ src/main/java/com/inti/entities/Avis.java
@@ -0,0 +1,19 @@
+package com.inti.entities;
+
+public class Avis {
+	
+	private Long idAvis;
+
+	public Avis() {
+		super();
+	}
+
+	public Long getIdAvis() {
+		return idAvis;
+	}
+
+	public void setIdAvis(Long idAvis) {
+		this.idAvis = idAvis;
+	}
+
+}
