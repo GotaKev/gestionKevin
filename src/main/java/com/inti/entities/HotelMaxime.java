ad94cb0d2956d8a6f0683c2c47928e0d46b84844
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2022-08-23 09:59:47 +0200

    renvoie Kevin

---
+++ src/main/java/com/inti/entities/HotelMaxime.java
@@ -0,0 +1,23 @@
+package com.inti.entities;
+
+public class HotelMaxime {
+
+	private int idHotel;
+
+	public HotelMaxime() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getIdHotel() {
+		return idHotel;
+	}
+
+	public void setIdHotel(int idHotel) {
+		this.idHotel = idHotel;
+	}
+	
+	
+	
+	
+}
